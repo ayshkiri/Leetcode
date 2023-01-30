8073dd97dd07d4187702636a5bc84f208a433a06
ayshkiri <alexshkirin@mail.ru>
2023-01-30 12:09:18 +0300

    Not important changes for test

--- [MID]RemoveStonesToMinimizeTheTotal.playground/Contents.swift
+++ [MID]RemoveStonesToMinimizeTheTotal.playground/Contents.swift
@@ -4,10 +4,10 @@ var arrayForResult: [Int] = piles
 var result = 0
 var max = 0
 for _ in 1...k {
-    arrayForResult.sort(by: >)
-    arrayForResult[0] -= arrayForResult[0] / 2
-//    max = arrayForResult.max()!
-//    arrayForResult[arrayForResult.firstIndex(of: max)!] -= max / 2
+    //arrayForResult.sort(by: >)
+    //arrayForResult[0] -= arrayForResult[0] / 2
+    max = arrayForResult.max()!
+    arrayForResult[arrayForResult.firstIndex(of: max)!] -= max / 2
 }
 for i in arrayForResult {
     result += i
