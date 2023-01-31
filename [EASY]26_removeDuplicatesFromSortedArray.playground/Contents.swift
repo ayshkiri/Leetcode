d7a29b3e83f704d485dadfed79cd70a8452b28a0
ayshkiri <alexshkirin@mail.ru>
2023-01-31 17:17:21 +0300

    Add better solution of [EASY]26_removeDuplicatesFromSortedArray

--- [EASY]26_removeDuplicatesFromSortedArray.playground/Contents.swift
+++ [EASY]26_removeDuplicatesFromSortedArray.playground/Contents.swift
@@ -1,7 +1,4 @@
 var nums = [0,0,1,1,1,2,2,3,3,4]
-
-// bad variant...
-
 var someSet: Set<Int> = []
 var k = 0
 for i in nums {
@@ -13,22 +10,3 @@ for i in someSet.sorted() {
 }
 nums
 k
-//
-
-
-
-var length = nums.count
-var i = 0
-while i < length - 1 {
-    if nums[i] == nums[i + 1] {
-        for j in i..<length - 1{
-            nums[j] = nums[j + 1]
-        }
-        length -= 1
-        
-    } else {
-        i += 1
-    }
-}
-length
-nums
