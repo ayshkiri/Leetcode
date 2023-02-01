192b9259f400765d399af6b238d69d5dcee554e3
ayshkiri <alexshkirin@mail.ru>
2023-02-01 16:30:09 +0300

    Add [Medium]80_removeDuplicatesFromSortedArrayII solution

--- [Medium]80_removeDuplicatesFromSortedArrayII.playground/Contents.swift
+++ [Medium]80_removeDuplicatesFromSortedArrayII.playground/Contents.swift
@@ -1,32 +1,65 @@
-//var nums = [0,1,1,1,2,3,3,4]
-var nums = [0,0,0,1,1,1,1,2,3,3]
+//position [0,1,2,3,4,5,6,7,8,9]
+//ar nums= [0,0,1,1,2,3,2,3,3,3]
+var nums = [0,0,1]
 var position = 0
-var isTwise = 1
-///////////////
-for i in 1..<nums.count {
-    if nums[i] == nums[position] && isTwise < 2 {
-        position += 1
-        isTwise += 1
-    }
-    if nums[i] > nums[position] {
-        isTwise = 1
+var isTwise = false
+if nums.count == 1 {
+    1
+}
+var k = 0
+var max = nums[0]
+for i in 1..<nums.count{
+    k += 1
+    print(k)
+    if nums[i] > max {
+        print("i = \(i), num[i] = \(nums[i]), position = \(position), isTwice = \(isTwise), num[pos] = \(nums[position]), max = \(max) - FIRST BEFORE")
+        max = nums[i]
+        isTwise = false
         position += 1
         nums[position] = nums[i]
+        print("i = \(i), num[i] = \(nums[i]), position = \(position), isTwice = \(isTwise), num[pos] = \(nums[position]), max = \(max) - FIRST AFTER")
+    } else if !isTwise {
+        print("i = \(i), num[i] = \(nums[i]), position = \(position), isTwice = \(isTwise), num[pos] = \(nums[position]), max = \(max) - SECOND BEFORE")
+        isTwise = true
+        position += 1
+        nums[position] = nums[i]
+        print("i = \(i), num[i] = \(nums[i]), position = \(position), isTwice = \(isTwise), num[pos] = \(nums[position]), max = \(max) - SECOND AFTER")
     }
 }
 nums
-position
+position + 1
+
+//for i in nums {
+//    print("\(k) итерация, i = \(i)")
+//    k += 1
+//
+//    if i > previous {
+//        print("i = \(i), position = \(position), isTwice = \(isTwise), num[pos] = \(previous) - FIRST BEFORE")
+//        previous = nums[position]
+//
+//        nums[position] = i
+//        position += 1
+//        isTwise = 1
+//        print("i = \(i), position = \(position), isTwice = \(isTwise), num[pos] = \(previous) - FIRST AFTER")
+//    } else if i == previous && isTwise == 1 {
+//        print("position = \(position), isTwice = \(isTwise), num[pos] = \(previous) - SECOND BEFORE")
+//        previous = nums[position]
+//        position += 1
+//        nums[position] = i
+//        isTwise += 1
+//        print("position = \(position), isTwice = \(isTwise), num[pos] = \(previous) - SECOND AFTER")
+//    }
+//}
+//position
+//nums
 
 /////////////////
 
 
 
 
-
-var k = 1
 for i in nums {
-    print("\(k) итерация, i = \(i)")
-    k += 1
+    
     if i > nums[position] {
         nums[position] = i
         position += 1
